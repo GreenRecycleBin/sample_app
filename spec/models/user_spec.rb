2aa40e67b516cc4ac98c6f4ef2162ca226083bb9
Daniel Le <GreenRecycleBin@gmail.com>
2014-04-18 14:41:08 +0800

    Add a test for email downcasing before saving

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,6 +71,17 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe 'email address with mixed case' do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe 'when password is not present' do
     before { @user.password = @user.password_confirmation = ' ' }
 
